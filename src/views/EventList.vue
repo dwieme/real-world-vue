17adefd2b5c8c152bac5cfea451aacf95f6d429e
Dan Wieme <dwieme@gmail.com>
2019-02-24 14:23:23 -0800

    Finish Modules

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link
         :to="{ name: 'event-list', query: { page: page - 1 } }"
@@ -9,11 +9,12 @@
         >Prev Page</router-link
       >
     </template>
-    <template v-if="eventsTotal > page * 3">
+    <span v-if="page != 1 && hasNextPage">|</span>
+    <template v-if="hasNextPage">
       <router-link
         :to="{ name: 'event-list', query: { page: page + 1 } }"
         rel="next"
-        >| Next Page</router-link
+        >Next Page</router-link
       >
     </template>
   </div>
@@ -27,9 +28,14 @@ export default {
   components: {
     EventCard
   },
+  data() {
+    return {
+      perPage: 3
+    }
+  },
   created() {
-    this.$store.dispatch('fetchEvents', {
-      perPage: 3,
+    this.$store.dispatch('event/fetchEvents', {
+      perPage: this.perPage,
       page: this.page
     })
   },
@@ -37,7 +43,10 @@ export default {
     page() {
       return parseInt(this.$route.query.page || 1)
     },
-    ...mapState(['events', 'eventsTotal'])
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['event', 'user'])
   }
 }
 </script>
